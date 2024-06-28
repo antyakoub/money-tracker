665299c45bb70f6006df0f89795e6c656b52bac9
tony <numbuh1@tonys-mbp.myfiosgateway.com>
2024-06-28 00:57:01 -0400

    Setup test express server

--- api/index.js
+++ api/index.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
+const port = 4040; //Define the port number to run the server on
 
+// Define a route for GET requests to '/api/test'
+app.get('/api/test', (req, res) => {
+    res.json({ body: 'test ok3' }); // Send a JSON response with the body 'test ok'
+});
+
+// Start the server and listen on the defined port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
