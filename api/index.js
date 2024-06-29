509f60371a021bc019d321ad116495d3bae16bf4
tony <numbuh1@tonys-MacBook-Pro.local>
2024-06-29 16:02:11 -0400

    Define route for GET/POST

--- api/index.js
+++ api/index.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 4040; //Define the port number to run the server on
+const port = 4040;
+
+app.use(cors());
+app.use(express.json()); // Middleware to parse JSON bodies
 
 // Define a route for GET requests to '/api/test'
-app.get('/api/test', (req, res) => {
-    res.json({ body: 'test ok3' }); // Send a JSON response with the body 'test ok'
+app.get('/api/test', (req,res) => {
+    res.json('test ok2');
 });
 
-// Start the server and listen on the defined port
+// Route handler for POST requests to '/api/transaction'
+app.post('/api/transaction', (req, res) => {
+    // Log the received data to console 
+    console.log('Received data:', req.body);
+    
+    // Send back the received JSON data
+    res.json(req.body);
+});
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
+
